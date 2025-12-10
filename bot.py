9a896baed517f999359a8d338bf58d308fc7a263
Pedrinhon <pedroalvescunha765@gmail.com>
2025-12-10 13:09:05 -0300

    Add new commands for emoji, gif, and member info

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from bot_logic import pass_gen
-
+from bot_emoji import emo_send
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -28,6 +28,25 @@ async def senha(ctx):
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+
+@bot.command()
+async def emoji(ctx):
+    await ctx.send("Seu emoji é" + emo_send())
+
+@bot.command()
+async def gif(ctx):
+    await ctx.send("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExeXpwcW9iZWI4bXc4eWNjbmg4aGN3dmgybm9jbzNsMmRyd3lrM2FmbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13hxeOYjoTWtK8/giphy.gif")
+
+@bot.command()
+async def membros(ctx):
+    guild = ctx.guild
+    await ctx.send(f'Esse servidor possui {guild.member_count} membros contando comigo!')
+
+@bot.command()
+async def id(ctx, member: discord.Member = None):
+    member = member or ctx.author
+    await ctx.send(f'O ID do membro {member.name} é {member.id}')
+
 
 @bot.command()
 async def joined(ctx, member: discord.Member):
